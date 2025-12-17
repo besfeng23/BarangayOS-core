867e8768f59973659fd36b4de2174e387ac5d120
Joven Ong <joven.ong23@gmail.com>
2025-12-17 17:26:01 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://api.qrs

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
         port: '',
         pathname: '/**',
       }
